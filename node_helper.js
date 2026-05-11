a0defe6b0f79af1e60cc59c728f02d294ab1f74d
Joe D <joseph.duchesneau@gmail.com>
2026-05-10 20:24:15 -0400

    Removed requests api and replaced with recommended NewsAPI. . Added linting.  Added code of conduct and change log.  Added automated workflow.  Added package.json and package-lock.json. (#6)

--- node_helper.js
+++ node_helper.js
@@ -1,44 +1,77 @@
-/* Magic Mirror
+/* MagicMirror²
  * Module: MMM-EveryNews
  *
  * By Mykle1
+ * Updated by themox
  *
  */
 const NodeHelper = require('node_helper');
-const request = require('request');
+const NewsAPI = require('newsapi');
 
 module.exports = NodeHelper.create({
 
     start: function() {
         console.log("Starting node_helper for: " + this.name);
+        this.config = null;
+        this.pendingNews = false;
     },
-    getNatGeo: function(url) {
-	// @TODO the request api is deprecated.  use a different API here to get these results.  Perhaps the NewsAPI module?
-        request({
-               url: 'https://newsapi.org/v2/top-headlines?sources='+this.config.source+'&pageSize=50&sortBy=popularity&apiKey='+this.config.apiKey,
-                headers: {'User-Agent':this.config.userAgent},
-            method: 'GET'
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body).articles;              
-                this.sendSocketNotification('NATGEO_RESULT', result);
+
+    // Gets news
+    getNews: function() {
+
+        if (!this.config || !this.config.sources || !this.config.apiKey) {
+            console.error("MMM-EveryNews: missing config", this.config);
+            return;
+        }
+        
+        var newsapi = new NewsAPI(this.config.apiKey);
+
+        newsapi.v2.topHeadlines({
+            sources: this.config.sources,
+            language: this.config.lang,  
+        }).then(response => {
+
+            if (response.status == "ok") {
+                var result = response.articles;              
+                this.sendSocketNotification('NEWS_RESULT', result);
+                return;
             } else {
                 // add error checking so the user gets a visible error they will need to do some troubleshooting
                 // this will be in lieu of the infinite loading indicator
-                var errorString = "Error loading NewsAPI data. <br>  Error # " + response.statusCode + ", " + JSON.parse(body).code + "<br>" + JSON.parse(body).message;
-                this.sendSocketNotification('NATGEO_ERROR', errorString);
+                var errorString = "Error loading NewsAPI data. <br>  Error # " + response.status;
+                this.sendSocketNotification('NEWS_ERROR', errorString);
+                console.log(response);
+                return;
             }
+
         });
+
     },
 
     socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GET_NATGEO') {
-            this.getNatGeo(payload);
+
+        // let API know we've received a config so we can go get the rest of the stuff
+        if (notification === 'CONFIG') {
+            this.config = payload;
+
+            if (this.pendingNews) {
+                this.pendingNews = false;
+                this.getNews();
+            }
+            return;
         }
 
-        if (notification === 'CONFIG') {
-                this.config = payload;
-                //console.log(this.config);
+        // defer trying to get the news until after the config is loaded
+        // sometimes MM tries to load this before there is a config it seems 
+        if (notification === 'GET_NEWS') {
+            if (!this.config) {
+                console.log("MMM-EveryNews: config not ready; deferring GET_NEWS");
+                this.pendingNews = true;
+                return;
+            }
+
+            this.getNews();
         }
+
     }
 });
